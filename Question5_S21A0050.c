2ed0a8fd36a94377ca631f2bb17a54e251aeede4
Nurul Athira <93875055+369HACK@users.noreply.github.com>
2021-12-14 16:46:28 +0700

    Add files via upload

---
+++ Question5_S21A0050.c
@@ -0,0 +1,21 @@
+//Documentation section
+//*declare and initialize their grade with the variable name as gradeAli,gradeAbu and gradeAminah*//
+
+//Preprocessor section
+#include<stdio.h>
+
+//Global Declaration section 
+ 
+//Main function section
+ int main(){
+
+      char gradeAli='A';
+      char gradeAbu='C';
+      char gradeAminah='D';
+
+      printf("Ali grade is= %c",gradeAli);
+      printf("Abu grade is= %c",gradeAbu);
+      printf("Aminah grade is= %C",gradeAminah);
+
+ }
+//User defined section
