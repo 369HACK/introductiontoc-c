968bda70a1deee4686317a133c6837665f4bc8fa
Nurul Athira <93875055+369HACK@users.noreply.github.com>
2021-12-14 16:49:42 +0700

    Add files via upload

---
+++ exercise41.c
@@ -0,0 +1,37 @@
+//documentation section
+/* write and solve this problem using a programming*/
+
+//pre-processor section
+#include<stdio.h>
+
+//global declaration section
+
+//main function section
+int main(){
+    int a=10,b=50,d;
+    float x=9.5;
+
+    d=a;  // a is 10
+    d +=b; 
+    printf("d +=b is %d\n", d);
+
+    d=a;
+    d*=a;
+    printf("d*a is %d\n",d);
+    
+    d=a;
+    d%=a;
+    printf("d % a is %d\n",d);
+
+    return 0;
+
+
+
+
+
+
+
+
+}
+
+//User defined section
