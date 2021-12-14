555e308cbb8d63b72d5e99fb37fb6dc569fb1f58
Nurul Athira <93875055+369HACK@users.noreply.github.com>
2021-12-14 16:48:06 +0700

    Add files via upload

---
+++ Question3_S21A0050.c
@@ -0,0 +1,24 @@
+//Documentation section
+//*Declare and initialize variable integerValue as 100,variable decimalValue as 0.125 and variable charValue as 'i'*//
+
+//Preprocessor section
+ #include<stdio.h>
+
+//Global declaratiom section
+
+//Main function section
+  int main(){
+      
+
+      int integerValue = 100;
+      float  decimalValue  = 0.125;
+      char charValue  ="i";
+
+      printf("The integer value is %d", integerValue);
+      printf("The decimal value is %f", decimalValue);
+      printf("The Char Value is %c",charValue);
+
+  }
+
+  //User defined sections
+  
