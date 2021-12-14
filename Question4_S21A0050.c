19195c9af372c238665d2a44e83726c85c5e11b9
Nurul Athira <93875055+369HACK@users.noreply.github.com>
2021-12-14 16:45:40 +0700

    Add files via upload

---
+++ Question4_S21A0050.c
@@ -0,0 +1,18 @@
+//Documentation section
+//*Declare and initialize variable with the number1=10 and number2=50*//
+
+//Preprocessor section
+#include<stdio.h>
+
+//Global declaration section
+
+// Main function section
+int main(){
+    int number1=10;
+    int number2=50;
+
+    printf("Value of number=%d", number1);
+    printf("value of number=%d",number2);
+}
+
+//User defined section
