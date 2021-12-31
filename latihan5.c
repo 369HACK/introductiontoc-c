c90cac6c7900dcdc413924c598536f696eb39def
Nurul Athira zukifli <93875055+369HACK@users.noreply.github.com>
2021-12-31 21:19:23 +0700

    Add files via upload

---
+++ latihan5.c
@@ -0,0 +1,18 @@
+//Documenmtation section
+//*declare and initialize radius of circle and constant pi*//
+
+//PRE-PROFESSOR SECTION
+#include<stdio.h>
+
+//Global declaration section
+ #define PI =3.142
+
+ //Main function section
+  int main(){
+      float myradiusofcircle=2.5;
+      double mypi=3.142;
+
+      printf("The radius is,%f",myradiusofcircle);
+      printf("The pi is,%lf",mypi);
+  }
+
